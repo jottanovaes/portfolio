513043495803df0ff17ac3ff132fdc269eafb784
jottanovaes <jottanovaes@gmail.com>
2023-08-04 15:03:25 -0300

    feat: started the navbar space
    create a space to put on the logo, the project links, contact and resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
-import Image from 'next/image'
+import Link from 'next/link'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
 export default function Home() {
   return (
-    <main className='flex justify-center'>
-      <h1>My project portfolio</h1>
+    <main className={`${inter.variable}`}>
+      <nav className='container flex justify-between py-2'>
+        <div className='logo'>logo?</div>
+        <ul className='flex gap-2'>
+          <li>
+            <Link href='#' className='block rounded px-4 py-3 leading-5'>
+              projects
+            </Link>
+          </li>
+          <li>
+            <Link href='#' className='block rounded px-4 py-3 leading-5'>
+              contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              href='#'
+              className='block h-12 rounded border-2 border-black bg-green-300 px-4 py-3 leading-5 hover:bg-green-400 hover:shadow-[2px_2px_0px_rgba(0,0,0,1)]'
+            >
+              resume
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </main>
   )
 }
